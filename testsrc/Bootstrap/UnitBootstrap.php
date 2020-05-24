94dfb48b1e80be100f1a757ce4ca2ff54e6123ec
Gordon Forsythe <gordon.forsythe@pagely.com>
2020-05-24 16:27:40 -0700

    move config files...some folder name refactoring for test coverage

--- testsrc/Bootstrap/UnitBootstrap.php
+++ testsrc/Bootstrap/UnitBootstrap.php
@@ -11,12 +11,12 @@ class UnitBootstrap extends AbstractBootstrap
 {
     public function prepare(?array $diConfigurators = [], ?array $middlewares = [], ?callable $routes = null): void
     {
-        array_unshift($diConfigurators, require dirname(__DIR__).'/files/diconfig.php');
-        array_unshift($middlewares, ...require dirname(__DIR__).'/files/middlewares.php');
+        array_unshift($diConfigurators, require dirname(__DIR__, 2).'/testassets/diconfig.php');
+        array_unshift($middlewares, ...require dirname(__DIR__, 2).'/testassets/middlewares.php');
         parent::prepare(
             $diConfigurators,
             $middlewares,
-            require dirname(__DIR__).'/files/routes.php'
+            require dirname(__DIR__, 2).'/testassets/routes.php'
         );
     }
 
